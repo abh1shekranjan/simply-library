50b2524b66dfdc1135a489828ec171e92d6694c4
satadhi <haldersatadhi@gmail.com>
2021-04-15 12:08:04 +0530

    implemented removeBook, started with borrowBook

---
+++ rest-api/BookListActions/borrowBook.js
@@ -0,0 +1,18 @@
+const { db_tables } = require("../config/db_config");
+const mongoose = require("mongoose");
+const { ERRORS } = require("../error");
+
+
+/**
+ * @description 
+ *      
+ * @param {object} req 
+ * @param {object} res 
+ */
+function borrowBook(req, res) {
+    res.status(200).send('not implemented yet by Satadhi');
+}
+
+module.exports = {
+    borrowBook
+}
