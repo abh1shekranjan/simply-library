c2e36c35022efab78af5d1703f962d80dc145313
Abhishek Ranjan <ranjan.abhishek.kotn@gmail.com>
2021-04-19 16:16:45 +0530

    User actions search-book and list-user-book added

--- rest-api/BookListActions/listUserBook.js
+++ rest-api/BookListActions/listUserBook.js
@@ -1,6 +1,29 @@
+const { db_tables } = require('../config/db_config');
+const mongoose = require('mongoose');
+const { ERRORS } = require('../error');
+
 function listUserBook(req, res) {
-  console.dir(req, { depth: null });
-  res.status(200).send('not implemented yet by Abhishek');
+  const user = mongoose.connection.collection(db_tables.USER_LIST);
+  Promise.resolve()
+    .then(() => {
+      users.findOne({
+        userId: req.user.id,
+      });
+    })
+    .then((result) => {
+      if (!result) {
+        return res.status(401).json({ msg: 'User not Authorized' });
+      } else {
+        res.json(result.borrowedBooks);
+      }
+    })
+    .catch((e) => {
+      if (e.message === ERRORS.BAD_REQUEST) {
+        return res.status(400).send('Bad request');
+      } else {
+        return res.status(500).send('Internal Error');
+      }
+    });
 }
 
 module.exports = {
--- rest-api/BookListActions/searchBook.js
+++ rest-api/BookListActions/searchBook.js
@@ -1,6 +1,48 @@
+const { db_tables } = require('../config/db_config');
+const mongoose = require('mongoose');
+const { ERRORS } = require('../error');
+// @route    POST /search-book
+// @desc     Search for a given book and return its book copy ids
+// @access   Private
 function searchBook(req, res) {
-  console.dir(req, { depth: null });
-  res.status(200).send('not implemented yet by Abhishek');
+  const userCollection = mongoose.connection.collection(db_tables.BOOK_LIST);
+  const { title, authors, publisher } = req.body;
+
+  // define mandatoryFields
+  const mandatoryFields = [title, authors, publisher];
+
+  Promise.resolve()
+    .then(() => {
+      // mandatory fields are missing
+      if (mandatoryFields.includes(undefined)) {
+        throw new Error(ERRORS.BAD_REQUEST);
+      }
+    })
+    .then(() =>
+      userCollection.findOne({
+        title: title,
+        authors: authors,
+        publisher: publisher,
+      })
+    )
+    .then((result) => {
+      if (result) {
+        //if book is found then return the book copies
+        res.json(result.bookCopy);
+      } else {
+        // throw a custom error
+        throw new Error(ERRORS.BOOK_NOT_FOUND);
+      }
+    })
+    .catch((e) => {
+      if (e.message === ERRORS.BAD_REQUEST) {
+        return res.status(400).send('Bad request');
+      } else if (e.message === ERRORS.BOOK_NOT_FOUND) {
+        return res.status(404).send('Book Not Found');
+      } else {
+        return res.status(500).send('Internal Error');
+      }
+    });
 }
 
 module.exports = {
